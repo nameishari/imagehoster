b40f43e241652bbb48279a85894cc80478d65cfa
nameishari <hari.tummala@auto1.com>
2019-06-14 11:25:11 +0530

    PARTB_Feature2 added comments table and removed comments in html files

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,28 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image image;
+}
